39d47ac019792b774dcf8c397f520c7453a2191f
Jhonatan Cruz <jcruz3@drew.edu>
2016-12-18 23:17:45 -0500

    organized code

--- getNews.py
+++ getNews.py
@@ -3,6 +3,7 @@ import urllib.request
 from twilio import twiml
 from twilio.rest import TwilioRestClient
 
+company= input("What company?")
 number= input("Give number: ")
 ACCOUNT_SID = "AC3bd3a69773f1ea33cb499e04597dcebe"
 AUTH_TOKEN = "5fc24bcb11f279efe7e608296bc74093"
@@ -20,5 +21,5 @@ client.messages.create(
     body= soup.p.text
     )
 
-#the following will ask what companies to grab info from 
+#the following will ask what companies to grab info from
     #media_url="https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg",
