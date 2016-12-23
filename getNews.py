06c6224237b0335d0b10d0f12c589a5297b8b55b
Jhonatan Cruz <jcruz3@drew.edu>
2016-12-22 23:47:44 -0500

    fixed file to be more organized

--- getNews.py
+++ getNews.py
@@ -4,48 +4,8 @@ import requests
 from twilio import twiml
 from twilio.rest import TwilioRestClient
 
-# twilioLogin= urllib.request.urlopen("https://www.twilio.com/login")
-# auth_handler = urllib.request.HTTPBasicAuthHandler()
-# auth_handler.add_password(realm='PDQ Application',
-#                           uri='https://mahler:8092/site-updates.py',
-#                           user='klem',
-#                           passwd='kadidd!ehopper')
-# print(twilioLogin.read(500))
-#ask for phone number
 
-# create a password manager
-# password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
-#
-# # Add the username and password.
-# # If we knew the realm, we could use it instead of None.
-# username="jcruz3@drew.edu"
-# password= "Mebigunot1!"
-# top_level_url = "https://www.twilio.com/login"
-# password_mgr.add_password(None, top_level_url, username, password)
-#
-# handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
-#
-# # create "opener" (OpenerDirector instance)
-# opener = urllib.request.build_opener(handler)
-#
-# # use the opener to fetch a URL
-# opener.open("https://www.twilio.com/login")
-#
-# # Install the opener.
-# # Now all calls to urllib.request.urlopen use our opener.
-# urllib.request.install_opener(opener)
-
-# auth_handler = urllib.request.HTTPBasicAuthHandler()
-# auth_handler.add_password(realm='none',
-#                           uri='https://www.twilio.com/login',
-#                           user='jcruz3@drew.edu',
-#                           passwd='')
-# opener = urllib.request.build_opener(auth_handler)
-# # ...and install it globally so it can be used with urlopen.
-# urllib.request.install_opener(opener)
-# urllib.request.urlopen('https://www.twilio.com/login')
-
-number= input("Give your number: ")
+number= "9082677299"
 
 #verify phone number through phone call
 ACCOUNT_SID = "AC3bd3a69773f1ea33cb499e04597dcebe"
