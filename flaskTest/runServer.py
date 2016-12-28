26cc60dd5429c688fa608b8a11f1a35295b0eaa6
Jhonatan Cruz <jcruz3@drew.edu>
2016-12-27 21:14:22 -0500

    homepage and verify program are now in one class

--- flaskTest/runServer.py
+++ flaskTest/runServer.py
@@ -1,44 +1,52 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, request
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
 app= Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
 
-@app.route('/verify')
-def verify():
-    EMAIL=""
-    PASSWORD=""
+class verifying:
+    @app.route('/verify')
+    def verify(self, number):
+        EMAIL="jcruz3@drew.edu"
+        PASSWORD="Mebigunot1!"
 
-    number= input("what is your number?")
-    driver= webdriver.Chrome()
-    driver.get("https://www.twilio.com/login")
+        self.number= first.phones()
+        # number= input("what is your number?")
+        driver= webdriver.Chrome()
+        driver.get("https://www.twilio.com/login")
 
-    email= driver.find_element_by_xpath("//*[@id='email']")
-    password= driver.find_element_by_xpath("//*[@id='password']")
-    email.send_keys(EMAIL)
-    password.send_keys(PASSWORD)
+        email= driver.find_element_by_xpath("//*[@id='email']")
+        password= driver.find_element_by_xpath("//*[@id='password']")
+        email.send_keys(EMAIL)
+        password.send_keys(PASSWORD)
 
-    login =driver.find_element_by_xpath("//*[@id='content']/div[2]/div/form/div[3]/div[1]/button").click()
-    #login.click()
-    time.sleep(5)
-    driver.get("https://www.twilio.com/console/phone-numbers/verified")
-    verify= driver.find_element_by_xpath("//*[@id='content']/div[3]/a").click()
-    #verify.click()
-    time.sleep(2)
-    phone=driver.find_element_by_id("PhoneNumber")
-    phone.send_keys(number)
-    time.sleep(5)
-    callMe =driver.find_element_by_xpath("//*[@id='verify-number']/div/div/div[3]/button[2]").click()
-    #callMe.click()
-    time.sleep(2)
-    veriCode= driver.find_element_by_class_name("text-danger")
-    print("Put the following digits on your phone when they call:"+ veriCode.text)
-    driver.quit()
+        login =driver.find_element_by_xpath("//*[@id='content']/div[2]/div/form/div[3]/div[1]/button").click()
+        #login.click()
+        time.sleep(5)
+        driver.get("https://www.twilio.com/console/phone-numbers/verified")
+        verify= driver.find_element_by_xpath("//*[@id='content']/div[3]/a").click()
+        #verify.click()
+        time.sleep(2)
+        phone=driver.find_element_by_id("PhoneNumber")
+        phone.send_keys(number)
+        time.sleep(5)
+        callMe =driver.find_element_by_xpath("//*[@id='verify-number']/div/div/div[3]/button[2]").click()
+        #callMe.click()
+        time.sleep(2)
+        veriCode= driver.find_element_by_class_name("text-danger")
+        # print("Put the following digits on your phone when they call:"+ veriCode.text)
+        driver.quit()
+
+    @app.route("/", methods=["GET", "POST"])
+    def index(self,phones):
+        if request.method== "POST":
+            self.phones= request.form["phoneNumber"]
+            verify()
+            # return render_template("/verify")
+
+        return render_template("/index.html")
 
 
 if __name__ =="__main__":
