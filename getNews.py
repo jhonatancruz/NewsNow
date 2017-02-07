dd56e9e2354d7fe65062a45ceac68a4ec518bd3d
Jhonatan Cruz <jcruz3@drew.edu>
2017-02-06 23:41:43 -0500

    changes made

--- getNews.py
+++ getNews.py
@@ -5,42 +5,42 @@ from twilio import twiml
 from twilio.rest import TwilioRestClient
 
 
-
-number= input("what is your number?")
-#verify phone number through phone call
-ACCOUNT_SID = "AC3bd3a69773f1ea33cb499e04597dcebe"
-AUTH_TOKEN = "5fc24bcb11f279efe7e608296bc74093"
-
-
-#ask for company
-company= input("What company?").lower()
-if company=="tesla":
-    sauce= urllib.request.urlopen('https://www.tesla.com/blog').read()
-    soup= bs.BeautifulSoup(sauce,'lxml')
-elif company=="verizon":
-    sauce= urllib.request.urlopen('http://www.verizon.com/about/news').read()
-    soup= bs.BeautifulSoup(sauce,'lxml')
-elif company=="apple":
-    sauce= urllib.request.urlopen('http://www.apple.com/newsroom/').read()
-    soup= bs.BeautifulSoup(sauce,'lxml')
-elif company=="samsung":
-    sauce= urllib.request.urlopen('https://news.samsung.com/us/').read()
-    soup= bs.BeautifulSoup(sauce,'lxml')
-elif company=="microsoft":
-    sauce= urllib.request.urlopen('http://news.microsoft.com/#sm.00zysa8e1ej9dds109q20m4ljtlni').read()
-    soup= bs.BeautifulSoup(sauce,'lxml')
-elif company=="amazon":
-    sauce= urllib.request.urlopen('https://commaai.blogspot.com/').read()
-    soup= bs.BeautifulSoup(sauce,'lxml')
-else:
-    print("Try Again")
-    company= input("What company?")
+def sendMessage():
+    number= input("what is your number?")
+    #verify phone number through phone call
+    ACCOUNT_SID = "AC3bd3a69773f1ea33cb499e04597dcebe"
+    AUTH_TOKEN = "5fc24bcb11f279efe7e608296bc74093"
 
 
-client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+    #ask for company
+    company= input("What company?").lower()
+    if company=="tesla":
+        sauce= urllib.request.urlopen('https://www.tesla.com/blog').read()
+        soup= bs.BeautifulSoup(sauce,'lxml')
+    elif company=="verizon":
+        sauce= urllib.request.urlopen('http://www.verizon.com/about/news').read()
+        soup= bs.BeautifulSoup(sauce,'lxml')
+    elif company=="apple":
+        sauce= urllib.request.urlopen('http://www.apple.com/newsroom/').read()
+        soup= bs.BeautifulSoup(sauce,'lxml')
+    elif company=="samsung":
+        sauce= urllib.request.urlopen('https://news.samsung.com/us/').read()
+        soup= bs.BeautifulSoup(sauce,'lxml')
+    elif company=="microsoft":
+        sauce= urllib.request.urlopen('http://news.microsoft.com/#sm.00zysa8e1ej9dds109q20m4ljtlni').read()
+        soup= bs.BeautifulSoup(sauce,'lxml')
+    elif company=="amazon":
+        sauce= urllib.request.urlopen('https://commaai.blogspot.com/').read()
+        soup= bs.BeautifulSoup(sauce,'lxml')
+    else:
+        print("Try Again")
+        company= input("What company?")
 
-client.messages.create(
-    to="+1"+number,
-    from_="+19088458499",
-    body= soup.p.text
-    )
+
+    client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+
+    client.messages.create(
+        to="+1"+number,
+        from_="+19088458499",
+        body= soup.p.text
+        )
