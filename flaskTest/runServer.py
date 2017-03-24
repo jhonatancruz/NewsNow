e9b1e0dc9fd8d0a53fce994f0d9e2bb0bdd543fa
Jhonatan Cruz <jcruz3@drew.edu>
2017-03-23 23:39:36 -0400

    updated with headless browser

--- flaskTest/runServer.py
+++ flaskTest/runServer.py
@@ -14,46 +14,8 @@ app= Flask(__name__)
 @app.route("/",methods=["GET", "POST"])
 def index(): #self,phones
     if request.method=="POST":
-        phone= request.form["phoneNumber"]
-        name= request.form["name"]
-
-        #this is where the application logs into twilio
-        EMAIL="jcruz3@drew.edu"
-        PASSWORD="Mebigunot1!"
-        global number
-        number=phone
-
-        driver= webdriver.Chrome()
-        driver.get("https://www.twilio.com/login")
-
-        email= driver.find_element_by_xpath("//*[@id='email']")
-        password= driver.find_element_by_xpath("//*[@id='password']")
-        email.send_keys(EMAIL)
-        password.send_keys(PASSWORD)
-
-        login =driver.find_element_by_xpath("//*[@id='content']/div[2]/div/form/div[3]/div[1]/button").click()
-        #login.click()
-        time.sleep(3)
-        driver.get("https://www.twilio.com/console/phone-numbers/verified")
-        #verify= driver.find_element_by_xpath("//*[@id='content']/div[3]/div[1]/a/span/i[2]").click()
-        verify= driver.find_element_by_xpath("//*[@id='content']/div[3]/a").click()
-        #for number after one number is verified //*[@id="content"]/div[3]/div[1]/a/span/i[2]
-        #for number before one number is verified //*[@id='content']/div[3]/a"
-        #verify.click()
-        time.sleep(2)
-        phone=driver.find_element_by_id("PhoneNumber")
-        phone.send_keys(number)
-        time.sleep(3)
-        callMe =driver.find_element_by_xpath("//*[@id='verify-number']/div/div/div[3]/button[2]").click()
-        #callMe.click()
-        time.sleep(2)
-        veriCode= driver.find_element_by_class_name("text-danger")
-        veriCode= veriCode.text
-        #grabs form infor
-
-        return render_template('nextPage.html', name=name, veriCode=veriCode )
-
-    return render_template("/index1.html")
+        return verifyPhone()
+    return render_template("/index.html")
 
 @app.route("/pickCompany", methods=["GET", "POST"])
 def pickCompany():
@@ -61,6 +23,45 @@ def pickCompany():
         return render_template("/companies.html")
 
     return render_template("/nextPage.html")
+
+def verifyPhone():
+
+    EMAIL="jcruz3@drew.edu"
+    PASSWORD="Mebigunot1!"
+
+    number=input("what is your number?")
+    driver= webdriver.PhantomJS()
+    driver.set_window_size(1124, 850) # set browser size.
+    driver.get("https://www.twilio.com/login")
+
+    email= driver.find_element_by_xpath("//*[@id='email']")
+    password= driver.find_element_by_xpath("//*[@id='password']")
+    email.send_keys(EMAIL)
+    password.send_keys(PASSWORD)
+
+    login =driver.find_element_by_xpath("//*[@id='content']/div[2]/div/form/div[3]/button[1]").click()
+    print("logged in")
+    time.sleep(3)
+    driver.get("https://www.twilio.com/console/phone-numbers/verified")
+    print("inside verified numbers tab")
+
+    verify= driver.find_element_by_xpath("//*[@id='content']/div[3]/div[1]/a/span/i[2]").click()
+    # verify= driver.find_element_by_xpath("//*[@id='content']/div[3]/a").click()
+    time.sleep(2)
+    phone=driver.find_element_by_id("PhoneNumber")
+    # time.sleep(2)
+    phone.send_keys(number)
+    print("about to call")
+    # time.sleep(5)
+    callMe =driver.find_element_by_xpath("//*[@id='verify-number']/div/div/div[3]/button[2]").click()
+    #callMe.click()
+    time.sleep(3)
+    veriCode= driver.find_element_by_class_name("text-danger")
+    print("Put the following digits on your phone when they call:"+ veriCode.text)
+    driver.quit()
+
+    return render_template('nextPage.html', name=name, veriCode=veriCode )
+
 
 @app.route("/verifyCompany", methods=["GET", "POST"])
 def verifyCompany():
